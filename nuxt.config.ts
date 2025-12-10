132580dcaa2caac224689c9a17ce7aee439989e9
Ali <alitalaatali30@gmail.com>
2025-12-10 18:55:28 +0200

    feat: Initialize Nuxt SaaS application with core pages, components, and content structure for documentation, blog, and changelog.

---
+++ nuxt.config.ts
@@ -0,0 +1,51 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxt/content',
+    '@vueuse/nuxt',
+    'nuxt-og-image'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  ui: {
+    theme: {
+      colors: ['primary', 'error', 'warning', 'success', 'info'],
+      transitions: true,
+      defaultVariants: {
+        color: 'neutral',
+        size: 'sm'
+      }
+    }
+  },
+  routeRules: {
+    '/docs': { redirect: '/docs/getting-started', prerender: false }
+  },
+
+  compatibilityDate: '2024-07-11',
+
+  nitro: {
+    prerender: {
+      routes: [
+        '/'
+      ],
+      crawlLinks: true
+    }
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
