d73522a97de60a051c551300e92b962184652173
Ali <alitalaatali30@gmail.com>
2025-12-17 12:35:00 +0200

    feat: Add favicon to the application using the new SVG logo.

--- app/app.vue
+++ app/app.vue
@@ -3,6 +3,7 @@ useHead({
   htmlAttrs: {
     lang: "en",
   },
+  link: [{ rel: "icon", type: "image/svg+xml", href: "/logo.svg" }],
   titleTemplate: (titleChunk) => {
     return titleChunk ? `${titleChunk} - Beon` : "Beon";
   },
